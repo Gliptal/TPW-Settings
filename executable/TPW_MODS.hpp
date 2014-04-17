688d654aeeaf7435908fb3861fab65702700f9b5
Mattia Affabris <affa@outlook.it>
2014-04-17 02:43:18 +0200

    fixed .gitignore

---
+++ executable/TPW_MODS.hpp
@@ -0,0 +1,122 @@
+// File created with TPW Settings. All comments are missing.
+//
+// It is better not to modify this file manually, because it may break TPW Setting parsing.
+//
+
+tpw_hint_active = 1;
+//><
+tpw_air_active = 1;
+tpw_air_delay = 10;
+tpw_air_time = 300;
+tpw_air_max = 20;
+//><
+tpw_animal_active = 1;
+tpw_animal_delay = 100;
+tpw_animal_max = 15;
+tpw_animal_maxradius = 200;
+tpw_animal_minradius = 75;
+tpw_animal_noisetime = 60;
+//><
+tpw_bleedout_active = 1;
+tpw_bleedout_inc = 5;
+tpw_bleedout_cthresh = 0.5;
+tpw_bleedout_pthresh = 0.7;
+tpw_bleedout_ithresh = 0.85;
+//><
+tpw_boat_active = 1;
+tpw_boat_delay = 10;
+tpw_boat_radius = 1000;
+tpw_boat_waypoints = 15;
+tpw_boat_num = 3;
+//><
+tpw_car_active = 1;
+tpw_car_delay = 10;
+tpw_car_waypoints = 15;
+tpw_car_num = 3;
+tpw_car_radius = 1000;
+tpw_car_nocombatspawn = 1;
+//><
+tpw_civ_active = 1;
+tpw_civ_delay = 5;
+tpw_civ_radius = 150;
+tpw_civ_waypoints = 15;
+tpw_civ_density = 5;
+tpw_civ_maxsquadcas = 4;
+tpw_civ_maxallcas = 50;
+tpw_civ_casdisplay = 0;
+tpw_civ_maxciv = 20;
+tpw_civ_interact = 15;
+tpw_civ_nocombatspawn = 1;
+//><
+tpw_ebs_active = 1;
+tpw_ebs_thresh = 5;
+tpw_ebs_delay = 1;
+tpw_ebs_debug = 0;
+tpw_ebs_radius = 500;
+tpw_ebs_playersup = 1;
+tpw_ebs_aisup = 1;
+tpw_ebs_findcover = 1;
+//><
+tpw_fall_active = 1;
+tpw_fall_sensitivity = 100;
+tpw_fall_threshold = 300;
+tpw_fall_delay = 10;
+tpw_fall_ragdoll = 1;
+tpw_fall_falltime = 20;
+//><
+tpw_fog_active = 1;
+tpw_fog_radius = 250;
+tpw_fog_delay = 5;
+tpw_fog_breath = 1;
+tpw_fog_groundfog = 1;
+tpw_fog_rainfog = 1;
+tpw_fog_heathaze = 1;
+tpw_fog_cansnow = 1;
+//><
+tpw_houselights_active = 1;
+tpw_houselights_delay = 10;
+//><
+tpw_hud_range[] = {25,500};
+tpw_hud_colour[] = {1,1,1};
+tpw_hud_enemycolour[] = {1,0.5,0};
+tpw_hud_squadcolour[] = {0,1,1};
+tpw_hud_alpha = 0.6;
+tpw_hud_asl[] = {1,0.6,0.45,1};
+tpw_hud_azt[] = {1,0.5,0.45,1};
+tpw_hud_grd[] = {1,0.4,0.45,1};
+tpw_hud_lmt[] = {1,0.4,0.5,1};
+tpw_hud_tmp[] = {1,0.6,0.5,1};
+tpw_hud_hlt[] = {1,0.4,0.55,1};
+tpw_hud_rng[] = {1,0.5,0.55,1};
+tpw_hud_vel[] = {1,0.6,0.55,1};
+tpw_hud_unit[] = {1,2,0.5,0.75};
+tpw_hud_offset[] = {0.35,0.28};
+tpw_hud_scale = 0.8;
+tpw_hud_textscale = 1;
+//><
+tpw_los_active = 0;
+tpw_los_debug = 0;
+tpw_los_maxdist = 100;
+tpw_los_mindist = 25;
+tpw_los_delay = 10;
+//><
+tpw_park_active = 1;
+tpw_park_perc = 25;
+tpw_park_createdist = 300;
+tpw_park_hidedist = 150;
+tpw_park_simdist = 20;
+tpw_park_max = 15;
+//><
+tpw_radio_active = 1;
+tpw_radio_house = 1;
+tpw_radio_car = 1;
+tpw_radio_time = 60;
+//><
+tpw_rain_active = 1;
+//><
+tpw_streetlights_active = 1;
+tpw_streetlights_factor = 10;
+tpw_streetlights_range = 1000;
+tpw_streetlights_colour = 0;
+tpw_streetlights_moths = 1;
+//><
